7e43d9d046cab601973dd0a9838e3f9738f97b58
Elias Keven <eliaskeven2@gmail.com>
2018-08-28 14:05:30 -0300

    corrigindo partes dos bugs

--- src/gerar-data.js
+++ src/gerar-data.js
@@ -3,7 +3,7 @@
 const anoInicial = 2018,
 	anoFinal = 2019,
 	mesInicial = 8,
-	mesFinal = 05,
+	mesFinal = 5,
 	diaSemana = [1,5]
 
 class Ano {
@@ -19,11 +19,11 @@ function gerarData () {
 	//gerando ano 
 	
 	//transformar em uma função recursiva construtora!
-	let anos = []
-	for (let anoAtual = anoInicial; anoInicial <= anoFinal; anoAtual++) {
-		if(anoAtual === anoInicial) 
+	const anos = []
+	for (let anoAtual = anoInicial; anoAtual <= anoFinal; anoAtual++) {
+		if (anoAtual === anoInicial) 
 			anos.push(new Ano(anoAtual, mesInicial))
-		else if (anoAtual === anoFinal)
+		else if (anoAtual === anoFinal) {
 				anos.push(new Ano(anoAtual, 12 - mesFinal))
 				//12 - mesFinal inverte o mes, para que quando for calcular o mes restante, de o numero exato de mes restante.		
 		} else {
@@ -47,43 +47,52 @@ function gerarData () {
 
 	anos.forEach(gerarMeses)
 
+	const nomearMes = mes => {
+		const nomeMeses = [ 
+			"Janeiro",
+			"Fevereiro",
+			"Março",
+			"Abril",
+			"Maio",
+			"Junho",
+			"Julho",
+			"Agosto",
+			"Setembro",
+			"Outrubro",
+			"Novembro",
+			"Dezembro"
+		]
+		return nomeMeses[mes - 1]
+	}
+
 	class Mes {
 		constructor (mes, dias) {
 			this.mes = mes
-			this.nome = nomearMes()
+			this.nome = nomearMes(mes)
 			this.dias = dias
 			//this.listaDias = []
 		}			
 	}
 
 	const criarMeses = (ano, mesAtual, totalMeses) => {
-			const meses = [],
-				  mes = mesAtual - (mesAtual - 1),
-				  date = new Date(ano, mesAtual, 0),
-
-			if (mesAtual > totalMeses) return meses
-			meses.push(new Mes(mes, date.getData()))
-		return criarMeses(ano, amesAtual + 1, 12)
+				console.log(mesAtual)
+		const meses = [],
+			  mes = mesAtual - (mesAtual - 1),
+			  date = new Date(ano, mesAtual, 0)
+		if (mesAtual > totalMeses) return meses
+		meses.push(new Mes(mes, date.getDate()))
+		return criarMeses(ano, mesAtual + 1, 12)
 	}
 
-	anos.forEach(ano => { //criarMeses:: mes -> array de meses restantes
-		ano.listaMeses = criarMeses(ano.ano, ano.meses)
+	anos.map(ano => { //criarMeses:: mes -> array de meses restantes
+		ano.listaMeses = criarMeses(ano.ano, 12 - mesInicial)
 	})
 
+	console.log(anos)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	//criar dias.
+	//bug em algum loop
+	//faltou inserir a quantidade de meses que resta no Ano
 }
+
+gerarData()
