72b62196fcee515da4209feb2ed6a3eda6949ac6
Elias Keven <eliaskeven2@gmail.com>
2018-09-05 16:03:14 -0300

    finalizando correção de bugs na criação do dia

--- src/gerar-data.js
+++ src/gerar-data.js
@@ -8,8 +8,7 @@ const anoFinal = {
 	ano: 2018,
 	mesFinal: 10
 }
-//explicação do erro: algo está somando o 1 com 2. dando 3
-const diasSelecionados = [1,6]
+const diasSelecionados = [0,5]
 
 function gerarData () {
 	class Ano {
@@ -65,7 +64,7 @@ function gerarData () {
 			"Julho",
 			"Agosto",
 			"Setembro",
-			"Outrubro",
+			"Outubro",
 			"Novembro",
 			"Dezembro"
 		]
@@ -88,37 +87,34 @@ function gerarData () {
 
 	//GERAR DIAS
 
-	//diaDaSemana:: Number -> Number
-	const diaDaSemana = diab => {
-		console.log('dia', diab)
+	//diaDaSemana:: Number -> String
+	const diaDaSemana = dia => {
 		const nomeDias = [
 			"Domingo","Segunda","Terça"," Quarta","Quinta","Sexta","Sábado"
 		]
-		return nomeDias[diab]
-		//-----------BUG ESTÁ AQUI!!!----------
+		return nomeDias[dia]
 	}
 
 	//verificarDia:: Number -> Number -> Boolean
-	const verificarDia = dias => diaRegistrado => {
-		//console.log(dia, diaRegistrado) 1
-		//console.log(diaDaSemana(dia - 1) === diaDaSemana(diaRegistrado))
-		return diaDaSemana(dias) === diaDaSemana(diaRegistrado) ? true : false
-	}
+	const verificarDia = dias => diaRegistrado => 
+		dias == diaRegistrado ? true : false
 
 	//diaAtual:: (Number, Number, Number) -> Number
 	const diaAtual = (ano, mes, dia) => {
-		console.log(ano, mes, dia)
-		const date = new Date
+		const date = new Date()
 		date.setFullYear(ano, mes - 1, dia)
-		return date.getDay() //1 segunda 1
+		return date.getDay()
 	}
 
 	//criarDia:: Objeto -> Objeto
 	const criarDia = ano => mes => {
 		const criar = dia => {
 			if (dia > mes.dias) return null
-			if (diasSelecionados.find(verificarDia(diaAtual(ano.ano, mes.mes, dia))))
-				mes.listaDias.push(new Dia(ano, mes, dia))
+			for (let contando = 0; contando < diasSelecionados.length; contando++) {
+				if (diasSelecionados[contando] == diaAtual(ano.ano, mes.mes, dia)) {
+					mes.listaDias.push(new Dia(ano, mes, dia))
+				}
+			}
 			return criar(dia + 1)
 		}
 		criar(1)
@@ -127,8 +123,8 @@ function gerarData () {
 	anos.forEach(ano => 
 		ano.listaMeses.forEach(mes => {
 			criarDia(ano)(mes)
-			//console.log(ano)
-			console.log(mes.listaDias)
+			console.log('-------------------', "\n", ano)
+			console.log('-------------------', "\n", mes)
 		})
 	)
 }
